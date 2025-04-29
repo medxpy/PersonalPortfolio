946ad5334b485851b938b22562bfb0f141adeeb0
medxpy <m3dh4ck@protonmail.ch>
2025-04-29 19:21:57 +0100

    fix: remove basePath and assetPrefix from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/PersonalPortfolio',
-  assetPrefix: '/PersonalPortfolio',
   eslint: {
     ignoreDuringBuilds: true,
   },
