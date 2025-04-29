c03f5f67e5b18f759f06d22b36220c4172077ace
medxpy <m3dh4ck@protonmail.ch>
2025-04-29 19:43:33 +0100

    fix: add basePath + assetPrefix for GH Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repo   = 'PersonalPortfolio'
+
 const nextConfig = {
-  output: 'export',
+  output: 'export',                               
+  basePath:    isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
